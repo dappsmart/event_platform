0ecf6c08dad3462057972d961d6bc453618520e8
dappsmart <96599016+dappsmart@users.noreply.github.com>
2024-02-03 19:45:18 +0300

    secondary user data unique:false  commit

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: false, unique: true },
+  username: { type: String, required: false, unique: false },
   firstName: { type: String, required: false },
   lastName: {type: String, required: false },
   photo: { type: String, required: false },
