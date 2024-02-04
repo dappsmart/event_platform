a7eec8194e9f4a02563779564aaa647b20013c5b
dappsmart <96599016+dappsmart@users.noreply.github.com>
2024-02-04 17:06:14 +0300

    type createuserparams and user schema for user object model  has been restrained to name, email and photo undone  commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,9 +7,8 @@ import User from '@/lib/database/models/user.model'
 import Order from '@/lib/database/models/order.model'
 import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
-
-
 import { CreateUserParams, UpdateUserParams } from '@/types'
+
 
 export async function createUser(user: CreateUserParams) {
   try {
--- types/index.ts
+++ types/index.ts
@@ -2,8 +2,8 @@
 export type CreateUserParams = {
     clerkId: string
     firstName: string
-   
-   
+    lastName: string
+    userName: string   
     email: string
     photo: string
   }
